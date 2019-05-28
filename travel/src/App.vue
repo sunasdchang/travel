3335b875c6d18eb21e1a17597ac1336d8bd7c0dd
sunasdchang <1729619432@qq.com>
2019-05-29 00:08:20 +0800

    project init

--- travel/src/App.vue
+++ travel/src/App.vue
@@ -1,8 +1,7 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<div id="app">
+  <router-view/>    <!--显示的是当前路由地址所对应的内容-->
+</div>
 </template>
 
 <script>
@@ -12,12 +11,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
